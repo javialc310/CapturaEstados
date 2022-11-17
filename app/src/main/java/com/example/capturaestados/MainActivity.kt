4bdd33ae3853e4cea5627867fe379aff05506166
javialc310 <javalc310@alu.centroafuera.es>
2022-11-17 13:37:15 +0100

    Guardado Estados

--- app/src/main/java/com/example/capturaestados/MainActivity.kt
+++ app/src/main/java/com/example/capturaestados/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.capturaestados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     private var onCreate=0
@@ -11,11 +12,15 @@ class MainActivity : AppCompatActivity() {
     private var onRestart=4
     private var onStop=5
     private var onDestroy=6
+    private lateinit var txtEmail:EditText
+    private lateinit var txtContra:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("onCreate $onCreate")
+        txtEmail=findViewById(R.id.editTextTextCorreo)
+        txtContra=findViewById(R.id.editTextTextPassword)
     }
 
     override fun onStart() {
@@ -46,5 +51,19 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         println("onDestroy $onDestroy")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle){
+        super.onSaveInstanceState(savedInstanceState)
+
+        savedInstanceState.putString("TAGEMAIL", txtEmail.text.toString())
+        savedInstanceState.putString("TAGCONTRA", txtContra.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        txtEmail.setText(savedInstanceState.getString("TAGEMAIL"))
+        txtContra.setText(savedInstanceState.getString("TAGCONTRA"))
     }
 }
